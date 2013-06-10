a2f7820220bbf9be2a60119992ec19bb0ebd96bd
dataskills <dataskills@gmail.com>
2013-06-10 15:51:39 -0700

    first template add

---
+++ protected/views/layouts/hnn-main-1.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]>
+<html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>
+<html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>
+<html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en" class="no-js"><!--<![endif]-->
+
+<head>
+
+    <!-- BEGIN: basic page needs -->
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=100%; initial-scale=1; maximum-scale=1; minimum-scale=1; user-scalable=no;"/>
+    <title>History News Network</title>
+    <!-- END: basic page needs -->
+
+    <!-- BEGIN: css -->
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/hnn/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/hnn/bootstrap-responsive.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/hnn/style.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/hnn/responsive.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/hnn/skin-default.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/hnn/prettyPhoto.css" rel="stylesheet" type="text/css"/>
+    <!-- END: css -->
+
+    <!-- BEGIN: js -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/google-code-prettify/prettify.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/modernizr.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/respond.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/superfish.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/hoverIntent.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.hoverdir.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.flexslider.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.elastislide.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.tweet.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/main.js"></script>
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!-- END: js -->
+
+</head>
+
+<body>
+
+<!-- .container -->
+<section class="container">
+
+<!-- #header .row -->
+<header id="header">
+    <div class="row">
+
+        <!-- .span3.logo -->
+        <div class="span6 logo">
+            <a href="./index.html"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/logo-gr.png" alt="Logo"/></a>
+        </div>
+        <!-- /.span3.logo -->
+
+        <!-- .span9 -->
+        <nav class="span6">
+
+            <!-- #menu -->
+            <ul id="menu">
+                <li><a href="#">HNN Information</a>
+                    <ul>
+                        <li><a href="#">Submissions</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Donations</a></li>
+                        <li><a href="#">Archives</a></li>
+                        <li><a href="#">Internships</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <!-- /#menu -->
+
+        </nav>
+        <!-- .span9-->
+
+    </div>
+</header>
+<!-- /#header .row -->
+
+<!-- div.navbar -->
+<div class="navbar">
+    <div class="navbar-inner">
+
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </a>
+        <a class="brand" href="#">Departments</a>
+
+        <div class="nav-collapse">
+            <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">News At Home</a></li>
+                <li><a href="#">News Abroad</a></li>
+                <li><a href="#">History</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Books</a></li>
+                <li><a href="#">Roundup</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blogs <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Mythic America</a></li>
+                        <li><a href="#">L&P</a></li>
+                        <li><a href="#">Gil Troy</a></li>
+                        <li><a href="#">Iwan Morgan</a></li>
+                        <li><a href="#">Josh Brown</a></li>
+                        <li><a href="#">Jim Loewen</a></li>
+                        <li><a href="#">Walid Phares</a></li>
+                        <li><a href="#">Stephanie Coontz</a></li>
+                        <li><a href="#">Steve Hochstadt</a></li>
+                        <li class="divider"></li>
+                        <li class="nav-header">Clio's Blog Roll</li>
+                        <li><a href="#">Part 1</a></li>
+                        <li><a href="#">Part 2</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-search pull-right" action="">
+                <input type="text" class="search-query span2" placeholder="Search">
+            </form>
+        </div>
+        <!-- /.nav-collapse -->
+
+    </div>
+    <!-- /navbar-inner -->
+</div>
+<!-- /div.navbar -->
+
+<!-- div#ad-top -->
+<div id="ad-top" class="row">
+    <div class="span3 center-content">
+        <a href="http://chnm.gmu.edu/donate/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/chmm-donate-black1.png"></a>
+    </div>
+    <div class="span6 center-content">
+        <a href="http://129.174.131.239/ad/redirect/138489/t151?url=home" target="_blank"><img
+                src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/history-channel-long-banner.jpg"></a>
+    </div>
+    <div class="span3 center-content">
+        <a href="http://www.americanheritage.com/" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/american-heritage.jpg"></a>
+    </div>
+</div>
+<!-- /div#ad-top -->
+
+<div class="divider-top"></div>
+
+<section id="main-container" class="row">
+    <div id="left-column" class="span3 center-content">
+        <!-- Twitter -->
+        <a class="twitter-timeline" href="https://twitter.com/myHNN" data-widget-id="343410152646524928">Tweets by
+            @myHNN</a>
+        <script>!function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                if (!d.getElementById(id)) {
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = p + "://platform.twitter.com/widgets.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }
+            }(document, "script", "twitter-wjs");</script>
+
+    </div>
+
+    <div id="center-column" class="span6">
+
+        <?php echo $content; ?>
+
+    </div>
+
+    <div id="right-column" class="span3 center-content">
+        <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/historiansrateobama.jpg"/></div>
+        <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/american-revolution-museum.jpg"/></div>
+        <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/wiebe-ad.jpg"/></div>
+        <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/cbmb-book.jpg"/></div>
+        <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/pipes-hnn.png"/></div>
+        <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/hnn/ad/aihe-ad-2.jpg"/></div>
+        <div class="contact-form-div">
+            <form class="well">
+                <h4>Join our mailing list</h4>
+                <input class="span2" type="email" placeholder="Your email address">
+                <span class="help-block">HNN updates for your inbox.</span>
+                <button type="submit" class="btn">Submit</button>
+            </form>
+        </div>
+    </div>
+</section>
+
+<!-- #footer.container -->
+<footer id="footer" class="container">
+
+    <!-- .row .clearfix -->
+    <div class="row clearfix">
+
+        <!-- .span3 -->
+        <div class="span3">
+
+            <h3 class="title">About Us</h3>
+
+            <p>Welcome to George Mason University's History News Network (which is popularly known as HNN).</p>
+
+            <p>Our mission is to help put current events into historical perspective. </p>
+
+        </div>
+        <!-- /.span3 -->
+
+        <!-- .span3 -->
+        <div class="span3">
+
+            <h3 class="title">Latest Tweets</h3>
+
+            <div class="tweet"></div>
+
+            <script type="text/javascript">
+                $(document).ready(function () {
+                    //TWITTER
+                    $(".tweet").tweet({
+                        join_text: "auto",
+                        username: "envato",
+                        avatar_size: 0,
+                        count: 2,
+                        auto_join_text_default: "we said,",
+                        auto_join_text_ed: "we",
+                        auto_join_text_ing: "we were",
+                        auto_join_text_reply: "we replied",
+                        auto_join_text_url: "we were checking out",
+                        loading_text: "loading tweets..."
+                    });
+                });
+            </script>
+
+        </div>
+        <!-- /.span3 -->
+
+        <!-- .span3 -->
+        <div class="span3">
+
+            <h3 class="title">Recent Post</h3>
+
+            <ul>
+                <li class="first">
+                    <span class="date">April 18, 2012</span>
+                    <a href="">HNN blog post </a>
+                </li>
+
+
+                <li>
+                    <span class="date">April 18, 2012</span>
+                    <a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a ligula mauris.</a>
+                </li>
+            </ul>
+
+        </div>
+        <!-- /.span3 -->
+
+        <!-- .span3 -->
+        <div class="span3">
+
+            <h3 class="title">Follow Us</h3>
+
+            <!-- .social -->
+            <ul class="social">
+                <li class="twitter"><a href="#">twitter</a></li>
+                <li class="facebook"><a href="#">facebook</a></li>
+                <li class="dribbble"><a href="#">dribbble</a></li>
+                <li class="vimeo"><a href="#">vimeo</a></li>
+                <li class="flickr"><a href="#">flickr</a></li>
+                <li class="pinterest"><a href="#">pinterest</a></li>
+            </ul>
+            <!-- /.social -->
+
+        </div>
+        <!-- /.span3 -->
+
+    </div>
+    <!-- /.row .clearfix -->
+
+    <!-- #copyright.clearfix -->
+    <div id="copyright" class="clearfix">
+
+        <p>Copyright <?php echo date('Y'); ?>. All rights reserved.</p>
+
+        <!-- #footer-menu -->
+        <nav id="footer-menu">
+            <ul class="clearfix">
+                <li><a href="#" class="current" data-description="Home Page">Newsletter</a>
+                </li>
+                <li><a href="#">Submissions</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Donations</a></li>
+                <li><a href="#">Archives</a></li>
+                <li><a href="#">Internships</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </nav>
+        <!-- /#footer-menu -->
+
+    </div>
+    <!-- /#copyright .clearfix -->
+
+</footer>
+<!-- /#footer .container -->
+
+
+</section>
+<!-- /.container -->
+
+</body>
+</html>
