a504716f64dbcdc5b64322dfaafdabe43766c856
dataskills <dataskills@gmail.com>
2013-07-06 12:27:19 -0700

    finished rbac

--- protected/models/User.php
+++ protected/models/User.php
@@ -16,93 +16,129 @@
  */
 class User extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return User the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return User the static model class
+     */
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'user';
-	}
+    private $oAuthManager = null;
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('created, login, status', 'numerical', 'integerOnly'=>true),
-			array('first_name, middle_name, last_name', 'length', 'max'=>32),
-			array('mail', 'length', 'max'=>64),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, mail, first_name, middle_name, last_name, created, login, status', 'safe', 'on'=>'search'),
-		);
-	}
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'user';
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'pass' => 'Pass',
-			'mail' => 'Mail',
-			'first_name' => 'First Name',
-			'middle_name' => 'Middle Name',
-			'last_name' => 'Last Name',
-			'created' => 'Created',
-			'login' => 'Login',
-			'status' => 'Status',
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('created, login, status', 'numerical', 'integerOnly' => true),
+            array('first_name, middle_name, last_name', 'length', 'max' => 32),
+            array('mail', 'length', 'max' => 64),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, mail, first_name, middle_name, last_name, created, login, status', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array();
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'pass' => 'Pass',
+            'mail' => 'Mail',
+            'first_name' => 'First Name',
+            'middle_name' => 'Middle Name',
+            'last_name' => 'Last Name',
+            'created' => 'Created',
+            'login' => 'Login',
+            'status' => 'Status',
+        );
+    }
 
-		$criteria->compare('id',$this->id,true);
-		$criteria->compare('pass',$this->pass,true);
-		$criteria->compare('mail',$this->mail,true);
-		$criteria->compare('first_name',$this->first_name,true);
-		$criteria->compare('middle_name',$this->middle_name,true);
-		$criteria->compare('last_name',$this->last_name,true);
-		$criteria->compare('created',$this->created);
-		$criteria->compare('login',$this->login);
-		$criteria->compare('status',$this->status);
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('id', $this->id, true);
+        $criteria->compare('pass', $this->pass, true);
+        $criteria->compare('mail', $this->mail, true);
+        $criteria->compare('first_name', $this->first_name, true);
+        $criteria->compare('middle_name', $this->middle_name, true);
+        $criteria->compare('last_name', $this->last_name, true);
+        $criteria->compare('created', $this->created);
+        $criteria->compare('login', $this->login);
+        $criteria->compare('status', $this->status);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    public function revokeAllRoles()
+    {
+        if ($this->oAuthManager === null)
+            $this->oAuthManager = Yii::app()->authManager;
+
+        $aUserRoleInfo = $this->oAuthManager->getAuthAssignMents($this->mail);
+
+        foreach ($aUserRoleInfo as $sUserRole => $oCAuthItem)
+        {
+            if (!$this->oAuthManager->revoke($sUserRole, $this->mail))
+            {
+                Throw new Exception("User role revoke action failed.");
+            }
+        }
+
+        return true;
+    }
+
+    public function assignRoles($aRole)
+    {
+        if ($this->oAuthManager === null)
+            $this->oAuthManager = Yii::app()->authManager;;
+
+        foreach ($aRole as $sRole)
+        {
+            if (!$this->oAuthManager->assign($sRole, $this->mail))
+            {
+                Throw new Exception("User role assign action failed.");
+            }
+        }
+
+        return true;
+    }
 }
--- protected/views/user/_form.php
+++ protected/views/user/_form.php
@@ -2,75 +2,134 @@
 /* @var $this UserController */
 /* @var $model User */
 /* @var $form CActiveForm */
+
+if ($this->action->id == 'update')
+{
+    $auth = Yii::app()->authManager;
+    $aRole = $auth->getRoles();
+    $aUserRoleInfo = $auth->getAuthAssignMents($model->mail);
+    $aUserRole = array();
+
+    foreach ($aUserRoleInfo as $sUserRole => $oCAuthItem)
+        $aUserRole[] = $sUserRole;
+}
 ?>
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'user-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+    <?php $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'user-form',
+        'enableAjaxValidation' => false,
+    )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo $form->errorSummary($model); ?>
+    <?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'pass'); ?>
-		<?php echo $form->passwordField($model,'pass',array('size'=>32,'maxlength'=>32)); ?>
-		<?php echo $form->error($model,'pass'); ?>
-	</div>
+    <?php if ($this->action->id == 'create'): ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'mail'); ?>
-		<?php echo $form->textField($model,'mail',array('size'=>60,'maxlength'=>64)); ?>
-		<?php echo $form->error($model,'mail'); ?>
-	</div>
+        <div class="row">
+            <?php echo $form->labelEx($model, 'pass'); ?>
+            <?php echo $form->passwordField($model, 'pass', array('size' => 32, 'maxlength' => 32)); ?>
+            <?php echo $form->error($model, 'pass'); ?>
+        </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'first_name'); ?>
-		<?php echo $form->textField($model,'first_name',array('size'=>32,'maxlength'=>32)); ?>
-		<?php echo $form->error($model,'first_name'); ?>
-	</div>
+    <?php elseif ($this->action->id == 'update'): ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'middle_name'); ?>
-		<?php echo $form->textField($model,'middle_name',array('size'=>32,'maxlength'=>32)); ?>
-		<?php echo $form->error($model,'middle_name'); ?>
-	</div>
+        <div class="row">
+            <?php echo CHtml::label('New Password', 'newPassword'); ?>
+            <?php echo CHtml::textField('User[newPassword]', '', array('size' => 32, 'maxlength' => 32)); ?>
+            <?php echo $form->error($model, 'pass'); ?>
+        </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'last_name'); ?>
-		<?php echo $form->textField($model,'last_name',array('size'=>32,'maxlength'=>32)); ?>
-		<?php echo $form->error($model,'last_name'); ?>
-	</div>
+    <?php endif; ?>
 
     <div class="row">
-        <?php echo $form->labelEx($model,'created'); ?>
-        <?php if($model->isNewRecord): ?>
-            <?php echo $form->textField($model,'created', array('value' => strtotime('now'))); ?>
-        <?php else: ?>
-            <?php echo $form->textField($model,'created'); ?>
-        <?php endif; ?>
-        <?php echo $form->error($model,'created'); ?>
+        <?php echo $form->labelEx($model, 'mail'); ?>
+        <?php echo $form->textField($model, 'mail', array('size' => 60, 'maxlength' => 64)); ?>
+        <?php echo $form->error($model, 'mail'); ?>
     </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'login'); ?>
-		<?php echo $form->textField($model,'login'); ?>
-		<?php echo $form->error($model,'login'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'first_name'); ?>
+        <?php echo $form->textField($model, 'first_name', array('size' => 32, 'maxlength' => 32)); ?>
+        <?php echo $form->error($model, 'first_name'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
-		<?php echo $form->error($model,'status'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'middle_name'); ?>
+        <?php echo $form->textField($model, 'middle_name', array('size' => 32, 'maxlength' => 32)); ?>
+        <?php echo $form->error($model, 'middle_name'); ?>
+    </div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'last_name'); ?>
+        <?php echo $form->textField($model, 'last_name', array('size' => 32, 'maxlength' => 32)); ?>
+        <?php echo $form->error($model, 'last_name'); ?>
+    </div>
 
-<?php $this->endWidget(); ?>
+    <?php if ($this->action->id == 'create'): ?>
+        <div class="row">
+            <?php echo $form->labelEx($model, 'created'); ?>
+            <?php if ($model->isNewRecord): ?>
+                <?php echo $form->textField($model, 'created', array('value' => strtotime('now'))); ?>
+            <?php else: ?>
+                <?php echo $form->textField($model, 'created'); ?>
+            <?php endif; ?>
+            <?php echo $form->error($model, 'created'); ?>
+        </div>
+
+        <div class="row">
+            <?php echo $form->labelEx($model, 'login'); ?>
+            <?php echo $form->textField($model, 'login'); ?>
+            <?php echo $form->error($model, 'login'); ?>
+        </div>
+
+        <div class="row">
+            <?php echo $form->labelEx($model, 'status'); ?>
+            <?php echo $form->textField($model, 'status'); ?>
+            <?php echo $form->error($model, 'status'); ?>
+        </div>
+
+    <?php elseif ($this->action->id == 'update'): ?>
+
+        <div class="row">
+            <?php echo $form->labelEx($model, 'created'); ?>
+            <?php echo $model->created; ?>
+        </div>
+
+        <div class="row">
+            <?php echo $form->labelEx($model, 'login'); ?>
+            <?php echo $model->login; ?>
+        </div>
+
+        <div class="row">
+            <?php echo $form->labelEx($model, 'status'); ?>
+            <?php echo $model->status; ?>
+        </div>
+
+        <?php if (!empty($aRole)): ?>
+
+            <div class="row">
+                <h2>User Role Assignment </h2>
+
+                <?php foreach ($aRole as $sRoleName => $oCAuthItem): ?>
+
+                    <?php echo CHtml::label($sRoleName, 'roles[' . $sRoleName . ']'); ?>
+                    <?php echo CHtml::checkBox('roles[' . $sRoleName . ']', (in_array($sRoleName, $aUserRole))); /* See if current role in user role array to see if should be checked */ ?>
+
+                <?php endforeach; ?>
+
+            </div>
+
+        <?php endif; ?>
+
+    <?php endif; ?>
+
+    <div class="row buttons">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
 
 </div><!-- form -->
