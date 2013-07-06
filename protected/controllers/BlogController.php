a7d623a09b813667554110bc149cf2d8d4674ef6
dataskills <dataskills@gmail.com>
2013-07-06 13:27:52 -0700

    finishing up rbac prep for jav

--- protected/controllers/BlogController.php
+++ protected/controllers/BlogController.php
@@ -37,8 +37,7 @@ class BlogController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				//'users'=>array('admin'),
-                'roles'=>array('hnnadmin'),
+				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
